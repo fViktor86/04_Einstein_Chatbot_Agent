2fd5c7d58d6d393d48f37b066ca6cdae30d000cd
fViktor86 <f.viktor1986@gmail.com>
2025-09-02 17:44:54 +0200

    Start the program. Read .env for API key. Setup llm. Get first responses from llm.

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_key,
+    temperature=0.5
+)
+
+response = llm.invoke([{
+    "role":"user",
+    "content":"Hi there, how are you?"
+}])
+
+print(response.content)
+# print("Hi, I am Albert, how can I help you today?")
+#
+# while True:
+#     user_input = input("You: ")
+#
+#     if user_input == "exit":
+#         break
+#
+#     print(f"Cool, thanks for sharing that {user_input}")
